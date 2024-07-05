f1d96793012ef1023d60bbe1abec19c7305801e9
Konrad Kosmatka <kkonrad92@gmail.com>
2024-07-05 23:16:50 +0200

    Add configuration preset for TEF668X Headless USB Tuner

---
+++ presets/tef-headless/Config.hpp
@@ -0,0 +1,73 @@
+#ifndef FMDX_TUNER_CONFIG_H
+#define FMDX_TUNER_CONFIG_H
+
+/* -----------------------------------------------------------------------
+   Generic tuner configuration
+   ----------------------------------------------------------------------- */
+/* Tuner driver (available drivers: TEF668X, SAF7730, SAF775X) */
+/* Warning: SAF775X support is currently very experimental */
+#define TUNER_DRIVER TEF668X
+
+/* I2C pull-ups in Arduino AVR driver */
+/* Enable only on Arduino boards modified to 3.3V operation
+   For 5V boards use external 10 kΩ pull-ups from SDA and SCL to 3.3V */
+#define TUNER_AVR_PULLUP false
+/* ----------------------------------------------------------------------- */
+
+
+/* -----------------------------------------------------------------------
+   Audio configuration
+   ----------------------------------------------------------------------- */
+#define AUDIO_I2S_ENABLED true
+/* ----------------------------------------------------------------------- */
+
+
+/* -----------------------------------------------------------------------
+   Antenna switch configuration
+   ----------------------------------------------------------------------- */
+#define ANTENNA_ENABLED 1
+
+/* Antenna driver (available drivers: Antenna, AntennaBCD) */
+#define ANTENNA_DRIVER AntennaBCD
+
+#define ANTENNA_COUNT 2
+#define ANTENNA_PINS  PB6
+
+/* Delay of the antenna switch [miliseconds] */
+#define ANTENNA_SWITCH_DELAY 50
+/* ----------------------------------------------------------------------- */
+
+
+/* -----------------------------------------------------------------------
+   Rotator controller configuration
+   ----------------------------------------------------------------------- */
+#define ROTATOR_ENABLED 0
+#define ROTATOR_PIN_CW  6
+#define ROTATOR_PIN_CCW 7
+
+/* Automatic rotator stop after specified time [seconds] */
+#define ROTATOR_TIMEOUT 90
+
+/* Rotator delay between direction change [miliseconds] */
+#define ROTATOR_DELAY 1000
+/* ----------------------------------------------------------------------- */
+
+
+/* -------------------------------------------------------------------------
+   External button configuration
+   ----------------------------------------------------------------------- */
+#define BUTTON_ENABLED 0
+#define BUTTON_PIN 13
+
+/* Button debounce time [miliseconds] */
+#define BUTTON_DEBOUNCE 50
+/* ----------------------------------------------------------------------- */
+
+
+/* -----------------------------------------------------------------------
+   Serial port configuration
+   ----------------------------------------------------------------------- */
+#define SERIAL_PORT_SPEED 115200
+/* ----------------------------------------------------------------------- */
+
+#endif
---
+++ presets/tef-headless/ConfigTEF668X.hpp
@@ -0,0 +1,41 @@
+#ifndef FMDX_TUNER_CONFIG_TEF668X_H
+#define FMDX_TUNER_CONFIG_TEF668X_H
+
+/* -----------------------------------------------------------------------
+   TEF668X configuration
+   ----------------------------------------------------------------------- */
+/* Reference clock frequency, supported values:
+ * 0 - GPIO selected 4/9.216 MHz
+ * 4000000 - 4.000 MHz
+ * 9216000 - 9.216 MHz
+ * 12000000 - 12.000 MHz
+ * 55466670 - 55.46667 MHz */
+#define TUNER_TEF668X_REF_CLOCK 55466670
+
+/* Reference clock selection pin (GPIO) */
+#define TUNER_TEF668X_PIN_REF_CLOCK 15
+
+/* RDS DAVN GPIO mode (true) or I2C polling (false) */
+#define TUNER_TEF668X_RDS_DAVN true
+/* RDS DAVN pin (GPIO) */
+#define TUNER_TEF668X_PIN_RDS_DAVN PB7
+
+/* DAC configuration via GPIO */
+#define TUNER_TEF668X_DAC_CONF true
+/* DAC configuration pin (GPIO) */
+#define TUNER_TEF668X_PIN_DAC_CONF PB3
+
+/* Versions of tuner patches embedded into firmware
+   Warning: There is no enough space on AVR for all of them at once */
+#define TUNER_TEF668X_PATCH_V101 0
+#define TUNER_TEF668X_PATCH_V102 0
+#define TUNER_TEF668X_PATCH_V205 1
+
+/* FM signal level offset for calibration */
+#define TUNER_TEF668X_FM_LEVEL_OFFSET -7.5
+
+/* AM signal level offset for calibration */
+#define TUNER_TEF668X_AM_LEVEL_OFFSET 0
+/* ----------------------------------------------------------------------- */
+
+#endif
